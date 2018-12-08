8ade0de29aef8ecf55180c68c3cd1dbfb1f270f4
unknown <w.andrew.pedersen@gmail.com>
2018-12-08 15:58:29 -0700

    database deployed- snacks table schemas and seeds

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const queries = require('./queries')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 5556
+
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send("Hello world !!"))
+app.get('/', (req, res) => {
+    queries.listAll().then(snacks => res.send(snacks))
+})
 
 app.listen(port, console.log(`This shit getting rowdy on PORT ${port} !!`))
--- database-connection.js
+++ database-connection.js
@@ -2,7 +2,7 @@
   const environment = process.env.NODE_ENV || 'development'
 
 
-  const config = require('../knexfile')
+  const config = require('./knexfile')
 
 
   const configEnv = config[environment]
